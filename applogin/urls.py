cc197fb2db989643da3fbfca6c10bb61272f66d6
Udit Pant <udit18049@iiitd.ac.in>
2019-07-30 15:48:37 +0530

    Initial commit.

---
+++ applogin/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'applogin'
+
+urlpatterns = [
+    url(r'^$', views.LoginFormView.as_view(), name='login'),
+]
---
+++ applogin/views.py
@@ -0,0 +1,38 @@
+from django.views import View
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
+
+class LoginFormView(View):
+    form_class = LoginForm
+    template_name = 'applogin/login_form.html'
+
+    # displays a blank form
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    # process form data
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+
+            user = form.save(commit=False)
+
+            # cleaned (normalized) data
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            # returns User objects if credentials are correct
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+
+                if user.is_active:
+                    login(request, user)
+                    # return
+
+        return render(request, self.template_name, {'form': form})
