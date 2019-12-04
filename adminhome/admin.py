9faeee9cab84d7c550c8fc55e65e4b805104d15c
root <root@2ad5e4aaddb7>
2019-12-04 15:44:46 +0530

    settings.py update remote

--- adminhome/admin.py
+++ adminhome/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import School, LocalAdmin, Student, Teacher, Principal, Parent
+from .models import School, LocalAdmin, Student, Teacher, Principal, Parent, Feecircular
 
 admin.site.register(School)
 admin.site.register(LocalAdmin)
@@ -7,3 +7,4 @@ admin.site.register(Student)
 admin.site.register(Teacher)
 admin.site.register(Principal)
 admin.site.register(Parent)
+admin.site.register(Feecircular)
